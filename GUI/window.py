bed161a051901480824d53cb20bc095ae5e980c8
Pranav <pranavcosmos4@gmail.com>
2021-08-23 19:04:54 +0530

    basic window with greetings message

---
+++ GUI/window.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window =  tk.Tk()   # creates a an instance of Tk() class (pops up a window)
+
+greeting = tk.Label(text="Hello there, Welcome to tkinterlkfjsldfjlksjdflksajflksjflkasjflksajflksjflksajflkajslfksdaj")
+greeting.pack()
+
+window.mainloop()   # makes the window stay on the screen (runs the window event loop)
