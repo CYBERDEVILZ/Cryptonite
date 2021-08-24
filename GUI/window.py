92cba50b5b8825d589de7a12e563bacb96a40f0b
Pranav <pranavcosmos4@gmail.com>
2021-08-25 05:10:13 +0530

    deleted unnecessary files

--- GUI/window.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window =  tk.Tk()                                           # creates a an instance of Tk() class (pops up a responsive window)
-
-greeting = tk.Label(text="Hello there, Welcome to tkinter") # creates a label
-greeting.pack()                                             # adds it to the window
-
-
-window.mainloop()                                           # runs the event loop and it listens for events.
