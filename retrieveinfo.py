bcf9facf4fc6fb3fca69091645e8cb80f5708b23
Pranav <pranavcosmos4@gmail.com>
2021-10-06 20:52:35 +0530

    minor change: code enhancement

--- retrieveinfo.py
+++ retrieveinfo.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect("Details.db")
 cursor = connection.cursor()
-
-details = cursor.execute("SELECT * FROM VICTIMS")
+    
+details = cursor.execute("SELECT * FROM VICTIMS")       
 data_list = details.fetchall()
 print(data_list)
