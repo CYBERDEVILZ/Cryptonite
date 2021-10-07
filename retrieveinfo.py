50361aeb52b486ddf6f949f8f795b3e12534b489
Pranav <pranavcosmos4@gmail.com>
2021-10-07 20:36:30 +0530

    added some extra components

--- retrieveinfo.py
+++ retrieveinfo.py
@@ -1,8 +1,19 @@
 import sqlite3
 
-connection = sqlite3.connect("Details.db")
-cursor = connection.cursor()
-    
-details = cursor.execute("SELECT * FROM VICTIMS")       
-data_list = details.fetchall()
-print(data_list)
+def victim_details():
+    connection = sqlite3.connect("Details.db")
+    cursor = connection.cursor()
+    details = cursor.execute("SELECT * FROM VICTIMS")       
+    data_list = details.fetchall()
+    return data_list
+
+def location():
+    details = victim_details()
+    lat_list = []
+    long_list = []
+    place_list = []
+    for detail in details:
+        lat_list.append(detail[4])
+        long_list.append(detail[5])
+        place_list.append(detail[6])
+    return (lat_list, long_list, place_list)
