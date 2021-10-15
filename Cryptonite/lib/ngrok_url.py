87ee8a2a66f5515a93f0ff437611bd20e9b12cbc
CYBERDEVILZ <pranavcosmos4@gmail.com>
2021-10-15 11:15:12 +0530

    Lib for spawning NGROK URL

--- Cryptonite/lib/ngrok_url.py
+++ Cryptonite/lib/ngrok_url.py
@@ -5,5 +5,5 @@ def create_tunnel():
         http_tunnel = ngrok.connect(8000, 'http')
         return(http_tunnel.public_url)
     except:
-        print("An error occurred while starting the ngrok tunnel :( \nPlease visit https://blahblah to see how you can solve this issue :)")
+        print("An error occurred while starting the ngrok tunnel :( \nPlease visit https://github.com/CYBERDEVILZ/Cryptonite/discussions/47 to see how you can solve this issue :)")
         return(None)
